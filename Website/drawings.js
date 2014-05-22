97eca8a38bee0746fb84e33a4d0c2f89b4aa4997
Jamie Hankins <jamiehankin@gmail.com>
2014-05-22 04:38:01 +0100

    Use circles as well to make the drawing more smooth. Also add a getImage function I can use to get the image

--- Website/drawings.js
+++ Website/drawings.js
@@ -12,14 +12,25 @@ $(function () {
         ctx.canvas.height = $(window).height();
     };
 
+    var getImage = function ()
+    {
+        var img = canvas.toDataURL("image/png");
+    }
+
     var initializeCvs = function (clearing) {
         var mnu = $(".menu")[0];
         ctx.lineCap = "round";
         resizeCvs();
+        ctx.lineWidth = 12;
         ctx.save();
         ctx.fillStyle = '#ffffff';
         ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
         ctx.restore();
+    };
+    ctx.fillCircle = function(x, y, radius) {
+            this.moveTo(x, y);
+            this.arc(x, y, radius, 0, Math.PI * 2, false);
+            this.fill();
     };
 
     $cvs.mousedown(function (e) {
@@ -27,7 +38,7 @@ $(function () {
             draw = 1;
             ctx.beginPath();
             ctx.moveTo(e.pageX - left, e.pageY - top);
-
+            ctx.fillCircle(e.pageX - left, e.pageY - top, 2);
         } else {
             draw = 0;
         }
@@ -36,6 +47,7 @@ $(function () {
             if (e.button === 0) {
                 draw = 0;
                 ctx.lineTo(e.pageX - left + 1, e.pageY - top + 1);
+                ctx.fillCircle(e.pageX - left + 1, e.pageY - top + 1, 2);
                 ctx.stroke();
                 ctx.closePath();
 
@@ -47,7 +59,7 @@ $(function () {
             if (draw === 1) {
                 ctx.lineTo(e.pageX - left + 1, e.pageY - top + 1);
                 ctx.stroke();
-
+                ctx.fillCircle(e.pageX - left, e.pageY - top, 2);
             }
         });
 
